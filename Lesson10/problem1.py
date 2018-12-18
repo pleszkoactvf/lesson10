d97ac0d0580764e6f92e3a36663a764d126a8df1
pleszkoactvf <43756310+pleszkoactvf@users.noreply.github.com>
2018-12-18 12:37:55 -0500

    Add files via upload

---
+++ Lesson10/problem1.py
@@ -0,0 +1,9 @@
+print('-'*65)
+print('Life Decision Bot: ')
+print()
+answer = input('Is it raining out? ')
+if answer == 'no': print('Go outside and do something!')
+
+
+
+
---
+++ Lesson10/problem3.py
@@ -0,0 +1,13 @@
+print('-'*65)
+print('Welcome to CLUB425, the most lit club in downtown ACTvF. Before I let you in, let me ask you a question...')
+print()
+age = input('What is your age today? ')
+age = int(age)
+
+if age >= 21:
+	print('Come on in')
+	print()
+
+else:
+	print('You are going to have to step back now. This is a 21+ club only so find somewhere else to party or find out what robot punches feel like.')
+	
